047f4d80298374b75a57f243b6add0804b6a5a50
Greg Kilmer <Remlikmg@comcast.net>
2020-01-15 23:31:22 -0500

    Reorganized constants and broke responses down by lines to handle multiple responses

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import socket
 import re
 
+#Server Details
 server = "irc.chat.twitch.tv"
 port = 6667
+
+#Botiun Authentication Stuffs
+botName = "Botiun"
 token = "oauth:2fsg30d1plxe20drjrb8s3lzwp91l6"
 clientId = "95kreu7tyixtgfqd9oe575hjttox0j"
+
+#Target Channels
 channels = ["patiun"]
-botName = "Botiun"
-adminName = "Patiun"
 
 irc = socket.socket()
 irc.connect((server, port))
@@ -20,18 +24,23 @@ for channel in channels:
     irc.send(f"JOIN #{channel}\n".encode('utf-8'))
 
 while True:
-    resp = irc.recv(2048).decode('utf-8')
+    respSet = irc.recv(2048).decode('utf-8')
 
-    if resp.startswith('PING'):
+    if respSet.startswith('PING'):
         irc.send("PONG\n".encode('utf-8'))
+    else:
+        print("New Response Set:")
+        print(respSet)
+        print("---------------------")
+        for resp in respSet.split('\n'):
+            if resp:
+                print("+")
+                print(resp)
+                eventType = 
+        print('\n')
 
-    tokens = resp.split(':')
-    if (len(tokens) > 2 and tokens[2]):
-        irc.send(f":tmi.twitch.tv USERNOTICE #patiun :message")
-
-    print(resp)
 
 #Tools for the job!
-#IRC = Chat, Join, Part, Badges and Stuff, (Host/Raid - a notice thing I need to figure out)
+#IRC = Chat, Action, Join, Part, Badges and Stuff, (Host/Raid - a notice thing I need to figure out)
 #Webhooks = Follow, Sub, Unsub, Go Live, End Stream, Mod Change, Extension tracking
 #PubSub = Bits, Bits Badges, Channel Points (Redeem), Subs(Again?), Commerce(Idk what this is), Whispers this user
